57bb4b1968d163b0ec58cf7a5e77a06c6edec7f6
varunvasisht84 <varun.vasisht84@gmail.com>
2017-08-18 11:32:40 +0530

    --> code to add the table entry. validation and all missing

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers
 {
@@ -16,6 +17,7 @@ namespace GigHub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -23,6 +25,33 @@ namespace GigHub.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+          Gig gig = new Gig();
+          string currentUserId = User.Identity.GetUserId();
+          ApplicationUser currentUser = _context.Users.First();//.FirstOrDefault(x => x.Id == currentUserId);
+          gig.Artist = currentUser;
+          gig.Venue = viewModel.Venue;
+          //gig.Id = 1;
+          gig.DateTime = DateTime.Now;
+
+          Genre genre = new Genre();
+          //genre.Id = 2;
+          genre.Name = "Jazz";
+
+          gig.Gernre = genre;
+
+          _context.Gigs.Add(gig);
+          _context.SaveChanges();
+
+          //gig.Artist = UserManager.FindById(User.Identity.GetUserId());
+          //gig.Artist = viewModel.a
+          //_context.Gigs.Add(new Gig() { Gernre = viewModel.Genre});
+          return RedirectToAction("Index", "Home");
         }
     }
 }
